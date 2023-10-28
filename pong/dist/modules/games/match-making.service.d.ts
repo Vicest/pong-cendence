f14152c7cc42b95fa275765ad20dced0e01e340c
Victor Martinez estrada <victormartinezestrada@gmail.com>
2023-10-28 10:24:48 +0200

    Removed dist folder

--- pong/dist/modules/games/match-making.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { QueuePlayerDto } from './dtos';
-export declare class MatchMakingService {
-    constructor();
-    joinQueue(queuedPlayer: QueuePlayerDto): boolean;
-    private queuedPlayers;
-}
--- pong/dist/modules/games/ongoing-games.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Server } from 'socket.io';
-export declare class OngoingGamesService {
-    constructor();
-    newMatch(matchKey: string, players: [string, string]): void;
-    update(gateway: Server, matchKey: string, player: string, actions: number): void;
-    tick(gateway: Server): void;
-    private logger;
-    private games;
-}
