cf828a239720a1f845229f11fa069ffd5a2ec3a4
Victor Martinez estrada <victormartinezestrada@gmail.com>
2023-10-29 15:31:43 +0100

    Restored back module imports,  oops

--- pong/src/back.module.ts
+++ pong/src/back.module.ts
@@ -3,13 +3,11 @@ import { Module } from '@nestjs/common';
 //Cosas de la api
 import { ApiModule } from './modules/api/api.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GamesModule } from './modules/games/games.module';
 import { DBconfig } from './modules/api/orm.config';
 
 @Module({
-	imports: [
-		ApiModule,
-		TypeOrmModule.forRoot(DBconfig)
-	],
+	imports: [ApiModule, GamesModule, TypeOrmModule.forRoot(DBconfig)],
 	controllers: [],
 	providers: []
 })
