e6b7d353dcd6e0eba21bdf78cb18ce63a51541de
Victor Martinez estrada <victormartinezestrada@gmail.com>
2023-11-15 23:18:34 +0100

    Merge preparations

--- pong/src/modules/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import  AuthController  from './auth.controller'
-
-@Module({
-  controllers: [],
-  providers: [AuthController],
-  exports: [AuthController],
-})
-export class AuthModule {}
--- pong/src/modules/chat/chat.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChatGateway } from './chat.gateway';
-
-@Module({
-  controllers: [],
-  providers: [ChatGateway],
-  exports: [ChatGateway],
-})
-export class ChatModule {}
