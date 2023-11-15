c1366bf4c1d98a6d20907bc7376a23591ad98786
Victor Martinez estrada <victormartinezestrada@gmail.com>
2023-11-15 02:55:50 +0100

    Just updated package to hod config in it, main.ts uses environment, mostly

--- pong/src/main.ts
+++ pong/src/main.ts
@@ -1,16 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { BackModule } from './back.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(BackModule);
-
+	const cs:ConfigService = app.get(ConfigService);
+	//We are not validating the .env file it seems (TODO)
+	const frontPort:number = cs.get<number>('FRONTEND_PORT') as number;
+	const frontUri:string = cs.get<string>('HOSTNAME') + ':' + frontPort;
 	//Configure CORS options
 	app.enableCors({
-		origin: ["http://localhost:5000", "http://localhost:3000" , "http://localhost:4200"],
+		origin: [frontUri, "http://localhost:3000", "http://localhost:4200"],//TODO Por qué tenemos el 5000, esto es back??
 		methods : "GET,HEAD,PUT,PATCH,POST,DELETE",
 	});
 
-	await app.listen(5000); //FIXME Use evironment PORT
+	await app.listen(frontPort);
 
 }
 
