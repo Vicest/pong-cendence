ef779b14527cdba72df9f754657ae21f9567a0b9
Juan <josuna-t@student.42madrid.com>
2023-12-29 18:30:02 +0100

    Added toggle to 2FA

--- frontend/src/routes/(authenticated)/app/profile/+page.svelte
+++ frontend/src/routes/(authenticated)/app/profile/+page.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
 	import { currentUser } from '../../../../store/Auth';
-	import { Avatar } from '@skeletonlabs/skeleton';
+	import { Avatar, SlideToggle } from '@skeletonlabs/skeleton';
 	import { faEdit } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 	import { Api } from '$services/api';
 
 	// Create a copy of the current user to edit
 	let currentUserCopy = { ...$currentUser };
-
 	let editMode: boolean = false;
 	let encodedImg: string;
 	let imageFile: any;
 	let twofaCode: string;
-
+	let value: boolean = currentUserCopy.two_factor_auth_enabled;
 	const handleFileChange = (event: any) => {
 		imageFile = event.target.files[0];
 
@@ -47,9 +46,10 @@
 			}
 		}
 	}
-
 	let edit2FABase64Qr: string | null = null;
-	async function get2FAData() {
+	async function get2FAData(e: any) {
+		const target = e.target as SlideToggle;
+		target.checked = $currentUser.two_factor_auth_enabled;
 		if (edit2FAMode) {
 			return;
 		}
@@ -60,23 +60,25 @@
 		edit2FAMode = true;
 	}
 
-	async function save2FAChanges() {
+	async function save2FAChanges(e: any) {
+		const target = e.target as SlideToggle;
 		if (!twofaCode) {
 			alert('Please enter the code');
 			return;
 		}
 		console.log(twofaCode);
-		Api.post('/auth/2FAcheck', {
+		Api.post('/auth/2FAchange', {
 			token: twofaCode
-		})
-			.catch((err) => {
-				console.log(err);
-			})
-			.finally(() => {
-				twofaCode = '';
+		}).then((res) => {
+			twofaCode = '';
+			if (res.status === 200) {
+				value = !$currentUser.two_factor_auth_enabled;
+				console.log('2fa status', currentUserCopy.two_factor_auth_enabled);
 				edit2FAMode = false;
-			});
+			}
+		});
 	}
+	async function Changetogglestatus() {}
 </script>
 
 <div class="container h-full mx-auto flex flex-col items-center">
@@ -134,6 +136,7 @@
 	<div class="flex flex-col justify-center items-center my-10 card w-full p-10">
 		<div class="flex gap-5 justify-center items-center mt-4 relative">
 			<span class="text-2xl font-bold">2FA</span>
+			<SlideToggle name="slide" on:click={get2FAData} bind:checked={value} />
 			<div class="ml-2 cursor-pointer" on:click={get2FAData} aria-hidden="true">
 				<Fa icon={faEdit} class="text-2xl" />
 			</div>
@@ -142,7 +145,9 @@
 			<div class="flex flex-col justify-center items-center mt-4">
 				<label class="label">
 					<span class="text-2xl font-bold 2fa-label">2FA</span>
-					<img src={edit2FABase64Qr} alt="2FA QR Code" />
+					{#if $currentUser.two_factor_auth_enabled == false}
+						<img src={edit2FABase64Qr} alt="2FA QR Code" />
+					{/if}
 					<input
 						class="input"
 						type="text"
